b57f9abb4faee7e06cca7270e3ece277cf7a4329
mario-gellrich-zhaw <60516564+mario-gellrich-zhaw@users.noreply.github.com>
2023-12-17 16:47:54 +0000

    ...

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 import os
 from datetime import datetime
 
-from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__, static_folder='static')
 
